8bce9cc0c8eaae8a4cce5d04a38cb3b479e52f9b
PrimeRig <you@example.com>
2024-04-23 21:04:37 +0200

    updated Category Model

--- Models/Category.js
+++ Models/Category.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const { boolean } = require("webidl-conversions");
 const categorySchama = new mongoose.Schema(
 {
-    categoryid:{type : int , required:true , unique : true},
+    categoryid:{type : Number , required:true , unique : true},
     categoryname:{type: String, required:true }
 },
 );
