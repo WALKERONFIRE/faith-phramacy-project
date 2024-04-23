a6136fb215d1d8ccc3f7c6e1a97f2097e2c39493
PrimeRig <you@example.com>
2024-04-23 20:37:40 +0200

    added delete, update, status routes

--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,62 @@
+const User = require("../Models/User");
+const bcrypt = require("bcrypt");
 const router = require("express").Router();
 
 
 
-//edit user (put)
+//UpdateUser
+router.put("/update/:id",async(req,res)=>{
+    if(req.body.password){
+        const salt = await bcrypt.genSalt();
+        req.body.password = await bcrypt.hash(req.body.password,salt);
+    }
+    try{
+        const updateUser = await User.findByIdAndUpdate(req.params.id,{
+            $set: req.body
+        },{new:true});
+        res.status(200).json(updateUser);
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
 
 
-//delete user (delete) 3
+//DeleteUser 
+router.delete("/delete/:id",async(req,res)=>{
+try{
+await User.findByIdAndDelete(req.params.id)
+res.status(200).json("User has been deleted")
+}catch(err){
+res.status(500).json(err)
+}
+});
 
-//get user by ID 2
+//GetUserStatus
+router.get("/status",async(req,res)=>{
+    const date = new Date();
+    const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
 
-//user stauts 
+    try{
+        const data = await User.aggregate([
+            {$match: {createdAt: {$gte: lastYear}}},
+            {
+                $project:{
+                    month: {$month: "$createdAt"},
+                },
+            },
+            {
+                $group:{
+                    _id: "$month",
+                    total: {$sum: 1},
+                },
+            }
+    ]);
+    res.status(200).json(data)
+    
+}catch(err){
+        res.status(500).json(err)
+    }
+})
 
-// get all users (Get) 1
 
 module.exports=router;
