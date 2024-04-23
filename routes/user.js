a79612516e349b43b2019224a6f4b14507cc6e12
Mariamasheraf120 <92663859+Mariamasheraf120@users.noreply.github.com>
2024-04-23 20:20:44 +0200

    Added Delete function by id

--- routes/user.js
+++ routes/user.js
@@ -30,3 +30,15 @@ router.get("/find/:id", async (req,res)=>{
         res.status(500).json(err);
     }
 });
+
+
+//DeleteUser 
+router.delete("/delete/:id",async(req,res)=>{
+    try{
+    await User.findByIdAndDelete(req.params.id)
+    res.status(200).json("User has been deleted")
+    }catch(err){
+    res.status(500).json(err)
+    }
+    });
+
